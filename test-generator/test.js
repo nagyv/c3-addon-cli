fe3d5c584a66f89d2b75797e6bd3b812997af38a
Nagy Viktor <viktor.nagy@gmail.com>
2019-03-14 22:57:41 +0100

    added test:gen command for simple generator testing

--- test-generator/test.js
+++ test-generator/test.js
@@ -2,7 +2,7 @@ import path from 'path'
 import test from 'ava'
 import sao from 'sao'
 
-const generator = path.join(__dirname, '..')
+const generator = path.join(__dirname, '..', 'src', 'generator')
 
 test('defaults', async t => {
   const stream = await sao.mock({ generator })
